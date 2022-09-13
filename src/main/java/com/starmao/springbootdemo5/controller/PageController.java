744f61f62badaf9b2bcf76b0e812ae987c2822fb
starmao <sparkmxx@gmail.com>
2022-09-13 17:19:01 +0800

    Initial commit

---
+++ src/main/java/com/starmao/springbootdemo5/controller/PageController.java
@@ -0,0 +1,6 @@
+package com.starmao.springbootdemo5.controller;/**
+ * @Author starmao
+ * @Date 2022/8/18 11:12
+ * @Version 1.0
+ */public class PageController {
+}
---
+++ src/main/java/com/starmao/springbootdemo5/domain/User.java
@@ -0,0 +1,6 @@
+package com.starmao.springbootdemo5.domain;/**
+ * @Author starmao
+ * @Date 2022/9/6 14:56
+ * @Version 1.0
+ */public class User {
+}
